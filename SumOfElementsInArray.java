0f06e19010a19eb68092c01f272ef69d405f6029
MANASAREDDY288 <jmanasareddy28@gmail.com>
2024-06-29 06:18:38 +0530

    Add files via upload

---
+++ SumOfElementsInArray.java
@@ -0,0 +1,17 @@
+package programs;
+
+public class SumOfElementsInArray {
+	public static void main(String[] args) {
+	int a[]= {10,20,50,60,30};
+	int sum=0;
+	for(int i=0;i<a.length;i++)
+	{
+		sum=sum+a[i];
+	}
+	System.out.println("sum of elements in array is:"+sum);
+	
+	
+			
+	}
+
+}
